e6500848b845275255c73ed4617efcb079abab6f
SavaKufarov <sava.kufarov@gmail.com>
2025-07-03 14:04:23 +0300

    BusinessLayer classes

---
+++ BusinessLayer/Absence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Absence
+    {
+        public int Id { get; set; }
+        public byte Type { get; set; }
+        public int DaysCount { get; set; }
+        public DateTime Created { get; set; }
+        public byte Status { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public User User { get; set; }
+    }
+
+}
---
+++ BusinessLayer/BusinessTrip.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BusinessTrip
+    {
+
+        public int Id { get; set; }
+        public byte Status { get; set; }
+        public DateTime IssueDate { get; set; }
+        public string ProjectName { get; set; } = null!;
+        public string UserFullName { get; set; } = null!;
+        public string? Task { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte TotalDays { get; set; }
+        public byte CarOwnership { get; set; }
+        public decimal Wage { get; set; }
+        public decimal AccomodationMoney { get; set; }
+        public string? CarBrand { get; set; }
+        public string? CarRegistrationNumber { get; set; }
+        public string CarTripDestination { get; set; } = null!;
+        public DateTime DateOfArrival { get; set; }
+        public string CarModel { get; set; } = null!;
+        public float? CarUsagePerHundredKm { get; set; }
+        public double? PricePerLiter { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string? ExpensesResponsibility { get; set; }
+        public DateTime Created { get; set; }
+
+        public User User { get; set; }
+
+    }
+
+}
